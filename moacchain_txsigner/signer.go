2d37f9603e9d9af7d6aa6c46b8e8917f9fae8a75
heshuchao <heshuchao@hotmail.com>
2019-07-08 14:46:16 +0800

    modify signer

--- moacchain_txsigner/signer.go
+++ moacchain_txsigner/signer.go
@@ -1,10 +1,10 @@
 package moacchain_txsigner
 
 import (
-	"encoding/hex"
+
 	"fmt"
 
-	"github.com/blocktree/go-owcdrivers/moacchainTransaction"
+	"github.com/blocktree/go-owcrypt"
 )
 
 var Default = &TransactionSigner{}
@@ -15,9 +15,10 @@ type TransactionSigner struct {
 // SignTransactionHash 交易哈希签名算法
 // required
 func (singer *TransactionSigner) SignTransactionHash(msg []byte, privateKey []byte, eccType uint32) ([]byte, error) {
-	signature, err := moacchainTransaction.SignRawTransaction(hex.EncodeToString(msg), privateKey)
-	if err != nil {
+	signature, retCode := owcrypt.Signature(privateKey, nil, 0, msg, 32, owcrypt.ECC_CURVE_SECP256K1)
+	if retCode != owcrypt.SUCCESS {
 		return nil, fmt.Errorf("ECC sign hash failed")
 	}
+
 	return signature, nil
 }
